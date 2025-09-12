d9f4a70767e60f034d3ae81b6b186353ce075a5d
Rodolfo Abadia <rabadiaf@gmail.com>
2025-09-12 15:17:59 -0600

    W

---
+++ check_health_200.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+LSE=${LSE:-http://localhost:4566}
+
+awsls() {
+  aws --endpoint-url="$LSE" "$@"
+}
+
+# ahora sí:
+awsls lambda update-function-configuration \
+  --function-name obs-health \
+  --environment "Variables={STATUS_CODE=200}"
+
